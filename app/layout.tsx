107b6a60f730ef52e09db3744fba7b56013e551a
Jeet Garala <155524426+GARALAJEET@users.noreply.github.com>
2025-06-12 00:11:16 +0530

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,8 @@
-// import { ThemeProvider } from "@/components/theme-provider"
-// import { cn } from "@/lib/utils"
-// import type { Metadata } from "next"
-// import { Inter } from "next/font/google"
-// import "./globals.css"
-// import type React from "react" // Import React
-
-// const inter = Inter({ subsets: ["latin"] })
-
-// export const metadata: Metadata = {
-//   title: "Jeet Garala ",
-//   description: "Full stack developer portfolio showcasing projects and skills",
-//     generator: 'v0.dev'
-// }
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   return (
-//     <html lang="en" suppressHydrationWarning>
-//       <body className={cn("min-h-screen bg-background font-sans antialiased", inter.className)}>
-//         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-//           {children}
-//         </ThemeProvider>
-//       </body>
-//     </html>
-//   )
-// }
 import { Inter } from "next/font/google"
 import "./globals.css"
 import type { Metadata } from "next"
 import { cn } from "@/lib/utils"
 import { ClientWrapper } from "./components/client_wrapper"
-
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -41,6 +10,26 @@ export const metadata: Metadata = {
   title: "Jeet Garala",
   description: "Full stack developer portfolio showcasing projects and skills",
   generator: "v0.dev",
+  icons: {
+    icon: [
+      {
+        url: '/images/icon.png',
+        sizes: 'any',
+      },
+      {
+        url: '/images/icon.png',
+        type: 'image/png',
+        sizes: '32x32',
+      }
+    ],
+    apple: [
+      {
+        url: '/images/icon.png',
+        type: 'image/png',
+        sizes: '180x180',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
